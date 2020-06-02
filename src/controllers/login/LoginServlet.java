8d85f082a72f5a0a00f430210d8ed1edecc77857
katsuya.ochi <enegori135@gmail.com>
2020-06-02 23:56:08 +0900

    Add login / loguout link

--- src/controllers/login/LoginServlet.java
+++ src/controllers/login/LoginServlet.java
@@ -20,8 +20,8 @@ import utils.EncryptUtil;
  */
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -30,67 +30,73 @@ public class LoginServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute("_token", request.getSession().getId());
-		request.setAttribute("hasError", false);
-		if(request.getSession().getAttribute("flush") != null) {
-		    request.setAttribute("flush",request.getSession().getAttribute("flush"));
-		    request.getSession().removeAttribute("flush");
-		}
-		
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/login/login.jsp");
-		rd.forward(request, response);
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    // ログイン画面を表示
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("_token", request.getSession().getId());
+        request.setAttribute("hasError", false);
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Boolean check_result = false;
-		
-		String code = request.getParameter("code");
-		String plain_pass = request.getParameter("plain_pass");
-		
-		Employee e = null;
-		
-		if(code != null && !code.equals("") && plain_pass != null && !plain_pass.equals("")) {
-		    EntityManager em = DBUtil.createEntityManager();
-		    
-		    String password = EncryptUtil.getPasswordEncrypt(
-		            plain_pass,
-		            (String)this.getServletContext().getAttribute("salt")
-		            );
-		    
-		    try {
-		        e = em.createNamedQuery("checkLoginCodeAndPassword", Employee.class)
-		              .setParameter("code",code)
-		              .setParameter("pass", password)
-		              .getSingleResult();
-		    } catch (NoResultException ex) {}
-		    
-		    em.close();
-		    
-		    if(e != null) {
-		        check_result = true;
-		    }
-		    
-		    if(!check_result) {
-		        request.setAttribute("_token", request.getSession().getId());
-		        request.setAttribute("hasError", true);
-		        request.setAttribute("code", code);
-		        
-		        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/login/login.jsp");
-		        rd.forward(request, response);
-		    } else {
-		        request.getSession().setAttribute("login_employee", e);
-		        
-		        request.getSession().setAttribute("flush", "ログインしました");
-		        response.sendRedirect(request.getContextPath() + "/");
-		    }
-		}
-	}
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/login/login.jsp");
+        rd.forward(request, response);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    // ログイン処理を実行
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 認証結果を格納する変数
+        Boolean check_result = false;
+
+        String code = request.getParameter("code");
+        String plain_pass = request.getParameter("password");
+
+        Employee e = null;
+
+        if(code != null && !code.equals("") && plain_pass != null && !plain_pass.equals("")) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            String password = EncryptUtil.getPasswordEncrypt(
+                    plain_pass,
+                    (String)this.getServletContext().getAttribute("salt")
+                    );
+
+            // 社員番号とパスワードが正しいかチェックする
+            try {
+                e = em.createNamedQuery("checkLoginCodeAndPassword", Employee.class)
+                      .setParameter("code", code)
+                      .setParameter("pass", password)
+                      .getSingleResult();
+            } catch(NoResultException ex) {}
+
+            em.close();
+
+            if(e != null) {
+                check_result = true;
+            }
+        }
+
+        if(!check_result) {
+            // 認証できなかったらログイン画面に戻る
+            request.setAttribute("_token", request.getSession().getId());
+            request.setAttribute("hasError", true);
+            request.setAttribute("code", code);
+
+            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/login/login.jsp");
+            rd.forward(request, response);
+        } else {
+            // 認証できたらログイン状態にしてトップページへリダイレクト
+            request.getSession().setAttribute("login_employee", e);
+
+            request.getSession().setAttribute("flush", "ログインしました。");
+            response.sendRedirect(request.getContextPath() + "/");
+        }
+    }
 
 }
